7c28d3aaf17411694bb6f542afd5fafe3a981d67
Andry Pebrianto <andrypeb227@gmail.com>
2023-12-08 16:43:42 +0700

    Add transaction to entity data source

--- database/data-source.ts
+++ database/data-source.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, Transaction } from "typeorm";
 import Env from "../src/utils/variables/Env";
 import { User } from "./entities/User";
 import { Avatar } from "./entities/Avatar";
@@ -15,7 +15,7 @@ export const PostgreDataSource = new DataSource({
   database: Env.DB_NAME,
   synchronize: Env.NODE_ENV === "prod" ? false : true,
   logging: Env.NODE_ENV === "prod" ? false : true,
-  entities: [User, Avatar, Question],
+  entities: [User, Avatar, Question, Transaction],
   migrations: [MigrationFile1702028107286],
   subscribers: [],
   ssl: Env.NODE_ENV === "prod" ? true : false,
