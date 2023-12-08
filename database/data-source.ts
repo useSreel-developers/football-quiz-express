9d26864d167065f33a50e1f9ff011f0e4fd89df4
Andry Pebrianto <andrypeb227@gmail.com>
2023-12-08 16:46:45 +0700

    Huh capek

--- database/data-source.ts
+++ database/data-source.ts
@@ -4,7 +4,7 @@ import Env from "../src/utils/variables/Env";
 import { User } from "./entities/User";
 import { Avatar } from "./entities/Avatar";
 import { Question } from "./entities/Question";
-import { MigrationFile1702028107286 } from "./migration/1702028107286-MigrationFile";
+import { MigrationFile1702028766957 } from "./migration/1702028766957-MigrationFile";
 
 export const PostgreDataSource = new DataSource({
   type: "postgres",
@@ -16,7 +16,7 @@ export const PostgreDataSource = new DataSource({
   synchronize: Env.NODE_ENV === "prod" ? false : true,
   logging: Env.NODE_ENV === "prod" ? false : true,
   entities: [User, Avatar, Question, Transaction],
-  migrations: [MigrationFile1702028107286],
+  migrations: [MigrationFile1702028766957],
   subscribers: [],
   ssl: Env.NODE_ENV === "prod" ? true : false,
 });
